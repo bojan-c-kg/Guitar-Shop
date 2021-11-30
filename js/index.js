a1571166c0b0ed5a2f2a90e6db0a123394015872
Bojan <95000816+bojan-c-kg@users.noreply.github.com>
2021-11-30 11:24:58 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+/*function newImg(){
+    document.slide.src = 'images/bass.jpg';
+}
+window.onload = newImg;*/
+
+var i = 0;
+    var images = [];
+    var time = 3000;
+
+            // Image list
+            images[0] = '/images/gs1.jpg';
+            images[1] = '/images/gs2.jpg';
+            images[2] = '/images/gs3.jpg';
+            images[3] = '/images/gs4.jpg';
+
+    // Change Image
+    function changeImg(){
+    document.slide.src = images[i];
+
+        if(i < images.length -1){
+                i++
+        }else {
+                i = 0
+        }
+        setTimeout("changeImg()", time);
+
+        }
+    window.onload = changeImg;
+
+console.log("here2")
+
+
+
+
+
+
